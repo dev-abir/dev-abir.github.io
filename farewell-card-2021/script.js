11d2b8d303f596f7c4b3e432ba68927f54e86ab3
dev-abir <gangulyabir777@gmail.com>
2021-08-26 15:59:46 +0530

    add quote

--- farewell-card-2021/script.js
+++ farewell-card-2021/script.js
@@ -5,7 +5,7 @@
 particlesJS("particles-js", {
     "particles": {
         "number": {
-            "value": 10,
+            "value": 30,
             "density": {
                 "enable": true,
                 "value_area": 800
